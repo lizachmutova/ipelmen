50360752d922e4e6900fc4c50ae9a9b1012dc4f9
lizachmutova <136744344+lizachmutova@users.noreply.github.com>
2023-08-23 14:19:46 +0300

    Сайт.

---
+++ advertisements_1/app_advertisements/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class AppAdvertisementsConfing(AppConfig):
+    
+    default_auto_field = 'django.db.models.BigAutoField'
+    name ="app_advertisements"
+    verbose_name = "Объявления"
+
+
